a54e78b8b8f4e83e10e43c16ca8347075c810d8e
Pirijan <hi@pirijan.com>
2022-10-12 20:05:11 -0400

    stub init, fetch libs

--- index.js
+++ index.js
@@ -5,8 +5,56 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 import { createServer } from 'http'
+import fetch from 'node-fetch'
+import { Headers } from 'node-fetch'
+import { URL, URLSearchParams } from 'url'
 
-createServer((req, res) => {
-  res.write('Hello World!')
-  res.end()
+let rules
+
+createServer((request, response) => {
+  response.write('kinopio-twitter-replies is online')
+  response.end()
 }).listen(process.env.PORT)
+
+
+console.log('☮️')
+
+// // https://developer.twitter.com/en/docs/tutorials/stream-tweets-in-real-time
+
+const init = async () => {
+  if (!rules) {
+    rules = await filterRules()
+  }
+  console.log('💖💖💖💖💖💖', rules)
+  // tweets()
+}
+const filterRules = async () => {
+  // Filtering criteria are applied to the filtered stream endpoints in the form of rules
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.TWITTER_API_BEARER_TOKEN}`
+  })
+  const body = {
+    add: [
+      { value: 'from:twitterdev from:twitterapi has:links' } // to:kinopioclub text:save
+      // https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/integrate/build-a-rule
+    ]
+  }
+  const url = 'https://api.twitter.com/2/tweets/search/stream/rules'
+  const options = { method: 'POST', headers, body: JSON.stringify(body) }
+  const response = await fetch(url, options)
+  const result = await response.json()
+  console.log('☃️☃️',result)
+  return result
+}
+// const tweets = async () => {
+
+// }
+
+
+
+// export default () {
+
+// }
+// init()
+init()
